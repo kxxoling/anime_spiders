6c6142960833ca7bc2e592c0ab483c9dfd0df974
kxxoling <kxxoling@gmail.com>
2017-04-08 23:37:36 +0800

    Define items.

--- anime_spiders/items.py
+++ anime_spiders/items.py
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Item, Field
 
 
-class AnimeSpidersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Torrent(Item):
+    site = Field()
+    id = Field()
+    title = Field()
+    team_name = Field()
+    team_id = Field()
+    size = Field()
+    torrent = Field()
+    magnet = Field()
+    link = Field()
+    pub_date = Field()
+    author = Field()
+    category = Field()
+
+
+class Anime(Item):
+    id = Field()
+    link = Field()
+    cover = Field()
+    name = Field()
+    orig_name = Field()
+    pub_date = Field()
+
+
+class CG(Item):
+    large_file_url = Field()
+    file_url = Field()
+    source = Field()
+    id = Field()
+    tags_string = Field()
+    md5 = Field()
+    pixiv_id = Field()
+
+
+class ShortVideo(Item):
+    id = Field()
+    md5 = Field()
+    preview_url = Field()
+    file_url = Field()
+    file_size = Field()
+    tags = Field()
+    author = Field()
+    source = Field()
+    score = Field()
+    file_ext = Field()

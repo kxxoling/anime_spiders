8118ac489ce82cebef6bb52691ed15c294f4cb67
kxxoling <kxxoling@gmail.com>
2017-06-11 02:01:56 +0800

    Remove useless comments.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import * as actions from './actions';
 import anime from './modules/anime';
 import cg from './modules/cg';
 
@@ -9,7 +8,6 @@ Vue.use(Vuex);
 const debug = process.env.NODE_ENV !== 'production';
 
 export default new Vuex.Store({
-  actions,
   modules: {
     anime, cg,
   },

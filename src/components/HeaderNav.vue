5f7c5e9523e49e2ff22048acfb24d4a48f15dbda
kxxoling <kxxoling@gmail.com>
2017-06-11 16:29:38 +0800

    Nav with icons.

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,7 +1,20 @@
 <template lang="jade">
 el-menu(theme="dark", mode="horizontal", router="true")
-  el-menu-item(index="/cg") CG
-  el-menu-item(index="/anime") Anime
-  el-menu-item(index="/shortvideo") Short Video
-  el-menu-item(index="/torrent") Torrent
+  el-menu-item(index="/cg")
+    i.fa.fa-picture-o
+    | CG
+  el-menu-item(index="/anime")
+    i.fa.fa-imdb
+    | Anime
+  el-menu-item(index="/shortvideo")
+    i.fa.fa-video-camera
+    | Short Video
+  el-menu-item(index="/torrent")
+    i.fa.fa-download
+    | Torrent
 </template>
+
+<style lang="stylus" scoped>
+i.fa
+  margin-right 0.5rem
+</style>

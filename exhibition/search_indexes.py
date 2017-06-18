7334345d7791b8afe40925a07a9a7374e341fdfe
kxxoling <kxxoling@gmail.com>
2017-06-19 00:05:48 +0800

    Simple index for torrents.

--- exhibition/search_indexes.py
+++ exhibition/search_indexes.py
@@ -2,7 +2,7 @@
 from haystack.indexes import SearchIndex, Indexable
 from haystack.indexes import CharField, DateTimeField
 
-from exhibition.models import Anime
+from exhibition.models import Anime, Torrent
 
 
 class AnimeIndex(SearchIndex, Indexable):
@@ -20,4 +20,24 @@ class AnimeIndex(SearchIndex, Indexable):
     def prepare(self, obj):
         data = super(AnimeIndex, self).prepare(obj)
         data['text'] = data['name']
+        return data
+
+
+class TorrentIndex(SearchIndex, Indexable):
+    text = CharField(document=True)
+    title = CharField(model_attr='title')
+    team_name = CharField(model_attr='team_name', null=True)
+    pub_date = DateTimeField(model_attr='pub_date')
+    author = CharField(model_attr='author', null=True)
+    category = CharField(model_attr='category', null=True)
+
+    def get_model(self):
+        return Torrent
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
+
+    def prepare(self, obj):
+        data = super(TorrentIndex, self).prepare(obj)
+        data['text'] = data['title']
         return data

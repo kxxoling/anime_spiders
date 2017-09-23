8115e9559ee3ac62bb386ba7796c710b373fc0a3
kxxoling <kxxoling@gmail.com>
2017-09-23 16:01:33 +0800

    new spider: PixivUserFirstPageSpider.

--- anime_spiders/spiders/pixiv.py
+++ anime_spiders/spiders/pixiv.py
@@ -46,8 +46,44 @@ class PixivIllustSpider(Spider):
             order += 1
 
 
+class PixivUserFirstPageSpider(Spider):
+    """ Scrape 1st page of Pixiv user
+    Not recommoned to use.
+    """
+    name = 'pixiv_user_1p'
+    allowed_domains = ['pixiv.net']
+    start_urls = [
+    ]
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'anime_spiders.pipelines.PixivDownloadPipeline': 100,
+            'anime_spiders.pipelines.DjangoItemPipeline': 200,
+        },
+    }
+
+    def parse(self, rsp):
+        """ Parse CG items from gallery page
+
+        @url
+        @returns items 1 200
+        @scraps crawled_from site_pk large_file_url file_url source
+        """
+        preview_images = rsp.xpath('//div[@class="newindex"]//ul[contains(@class, "ui-brick")]/li//img/@src').extract()
+        for image in preview_images:
+            pixiv_data = extract_pixiv_path(image)
+            large_file_url = 'https://i.pximg.net/img-original/img/{year}/{month}/{day}/{hour}/{minute}/{second}/{pk}_p{order}.jpg'.format(**pixiv_data)
+            yield CG(
+                crawled_from='pixiv.net',
+                site_pk=pixiv_data['pk'],
+                large_file_url=large_file_url,
+                file_url=large_file_url,
+                source='https://www.pixiv.net/member_illust.php?mode=medium&illust_id={pk}'.format(**pixiv_data),
+            )
+
+
 pixiv_ptn = re.compile(r'''
-https://i.pximg.net/c/\d{3,4}x\d{3,4}/img-master/img/
+https://i.pximg.net/c/\d{3,4}x\d{3,4}.{0,4}/img-master/img/
 (?P<year>\d+)/
 (?P<month>\d+)/
 (?P<day>\d+)/
